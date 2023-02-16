e42639c74e0c365daf2714447c26b854254df1b0
Joecthomsen <joecthomsen@gmail.com>
2023-02-16 16:33:48 +0100

    Pre day 3 push

---
+++ Day_2/Quiz_2.R
@@ -0,0 +1,19 @@
+200*0.04
+
+pbinom(2, 200, 0.004)
+
+n <- 9
+
+prop <- 2/9
+
+mean <- 1*prop + 2*prop + 3*prop
+mean
+(1-mean)^2*prop + (2-mean)^2*prop + (3-mean)^2*prop
+
+phyper(3, 3, 6, 3)
+
+3/10
+
+(3*(36))/20^2*17/19
+
+1-phyper(0, 2, 18, 3)
